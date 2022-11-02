c8beab418257479b013469d73f58e5efd960d7c0
mkimti <mokimti53@gmail.com>
2022-11-02 22:42:45 +0000

    Retrieved Tweets for GW4

--- SAFPLTeam.py
+++ SAFPLTeam.py
@@ -43,7 +43,7 @@ def populatePlayersTable(conn):
 
 def populatePlayersTweetsTable(conn):
     c = conn.cursor()
-    for i in range(3,4):
+    for i in range(4,5):
         dataframe1 = pd.read_csv(PATH + '/2021-22/gws/gw' + str(i) + '.csv')
         dataframe2 = pd.read_csv(PATH + '/2021-22/gws/gw' + str(i+1) + '.csv')
         tempEarliestDay = int(dataframe2.kickoff_time[0][8:10])
